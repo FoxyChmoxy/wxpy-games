7f5a1f27c0c1a2c0b34ac66cedd170c30bd64b00
Almas Zaurbekov <almasgaara@mail.ru>
2020-10-07 20:31:22 +0600

    circles project refactoring

---
+++ random-circles/ball.py
@@ -0,0 +1,9 @@
+class Ball:
+    def __init__(self, obj, x, y, rad, xDir, yDir, clr):
+        self.obj = obj
+        self.x = x
+        self.y = y
+        self.rad = rad
+        self.xDir = xDir
+        self.yDir = yDir
+        self.clr = clr
